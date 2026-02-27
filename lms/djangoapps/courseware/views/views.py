48edb4b21b3f577b2c8aa4af9e8274083a76d009
edxdeveloper <edxdeveloper@mindlogicx.com>
2026-02-27 18:32:12 +0000

    updated redirect

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -295,6 +295,7 @@ def courses(request):
     """
     Render "find courses" page.  The course selection work is done in courseware.courses.
     """
+    return redirect('/programs')
     courses_list = []
     course_discovery_meanings = getattr(settings, 'COURSE_DISCOVERY_MEANINGS', {})
     set_default_filter = ENABLE_COURSE_DISCOVERY_DEFAULT_LANGUAGE_FILTER.is_enabled()
